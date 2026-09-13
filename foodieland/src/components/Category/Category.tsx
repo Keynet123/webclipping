d9c9e89e3b624aa9e84e91286c684b0611d1db57
Keynet123 <viktorplay377@gmail.com>
2026-09-13 19:51:53 +0300

    Add Like

--- foodieland/src/components/Category/Category.tsx
+++ foodieland/src/components/Category/Category.tsx
@@ -14,7 +14,7 @@ function Category(props: CategoryInfo) {
             background: `linear-gradient(to bottom, ${props.color}00 0%, ${props.color}1A 100%)`,
         }}>
             <div className="category_slice">
-                <img src={props.icon}></img>
+                <img src={props.icon} className="category_logo"></img>
                 <span className="category_name">{props.text}</span>
             </div>
         </div>
